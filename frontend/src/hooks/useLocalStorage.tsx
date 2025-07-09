5831f5406edfed1a4d88813163dc96d66618eb7b
nicolasnna <nicolas.norambuenaa@gmail.com>
2025-07-09 01:44:44 -0400

    feat: agregar hook useLocalStorage para manejar categorías y transacciones en localStorage

---
+++ frontend/src/hooks/useLocalStorage.tsx
@@ -0,0 +1,41 @@
+import { Category, Transaction } from "@/types"
+
+type TypeCategories = 'categoryIncomes' | 'categoryExpenses'
+type TypeTransaction = 'incomes' | 'expenses'
+
+export function useLocalStorage(type: TypeTransaction | TypeCategories) {
+
+  const getValues = () => JSON.parse(localStorage.getItem(type) as string)
+
+  const addValue = (data: Category | Transaction) => {
+    const currentValues = getValues()
+    if (currentValues && currentValues?.length > 0) {
+      localStorage.setItem(type, JSON.stringify([...currentValues, data]))
+    } else {
+      localStorage.setItem(type, JSON.stringify(data))
+    }
+  }
+
+  const deleteValue = (id: string) => {
+    const currentValues = getValues()
+    if (currentValues && currentValues?.length > 0) {
+      const existing = currentValues.filter((c: Category | Transaction) => c.id !== id)
+      localStorage.setItem(type, JSON.stringify(existing))
+    }
+  }
+
+  const updateValue = (id: string, data: Category | Transaction) => {
+    const currentValues = getValues()
+    if (currentValues && currentValues?.length > 0) {
+      const updated = currentValues.map((c: Category | Transaction) => c.id === id ? {...c, ...data} : c)
+      localStorage.setItem(type, JSON.stringify(updated))
+    }
+  }
+
+  return {
+    getValues,
+    addValue,
+    deleteValue,
+    updateValue
+  }
+}
