6e077b161a385cee1674d32f3c46ab58c306b048
nicolasnna <nicolas.norambuenaa@gmail.com>
2025-07-15 17:12:25 -0400

    Console log cwd

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -19,7 +19,7 @@ app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
         '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
     customCssUrl: CSS_URL,
 }))
-
+console.log(process.cwd())
 app.use(cors({
   origin: 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
