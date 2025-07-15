a35d0b6d4d57784a457e431b4a37a647cb39d3dc
nicolasnna <nicolas.norambuenaa@gmail.com>
2025-07-15 17:44:17 -0400

    Fix version

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,7 +13,12 @@ const app = express()
 app.use(cookieParser())
 app.use(express.json())
 
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css"
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
+    customCss:
+        '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+    customCssUrl: CSS_URL,
+}))
 app.use(cors({
   origin: 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
